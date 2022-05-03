f55bdbcb51f115b0aad8ccb68b1d64a923961f96
edujpmouta <62976840+edujpmouta@users.noreply.github.com>
2022-05-03 02:42:45 -0300

    novos valores de gps

--- script/Mapa.js
+++ script/Mapa.js
@@ -5,10 +5,10 @@ AFRAME.registerComponent('cursor-listener', {
             let id = this.id;
             switch (id) {
                 case "ponto1":
-                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Quadra")
+                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Garagem 68")
                     break;
                 case "ponto2":
-                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Auditorio")
+                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Casa 70")
                     break;
                 case "ponto3":
                     texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Biblioteca")
