f4189471b59fe28f2223ccfe1447fa30db899cd4
edujpmouta <62976840+edujpmouta@users.noreply.github.com>
2022-05-03 00:32:51 -0300

    Atualização do mapa

--- script/script.js
+++ script/script.js
@@ -1,27 +1,25 @@
-AFRAME.registerComponent('cursor-listener', {
-    init: function () {
-        this.el.addEventListener('mouseenter', function () {
-            let texto = document.querySelector("#texto");
-            let id = this.id;
-            switch (id) {
-                case "ponto1":
-                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Ponto 1")
-                    break;
-                case "ponto2":
-                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Ponto 2")
-                    break;
-                case "ponto3":
-                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Ponto 3")
-                    break;
-                case "ponto4":
-                    texto.setAttribute("text", "width: 1; lineHeight: 50; letterSpacing: 5; color: white; align: center ; value: Ponto 4")
-            }
-            texto.setAttribute("visible", true);
-        });
+window.onload = () => {
+    let lat;
+    let long;
+    let marker;
+    navigator.geolocation.getCurrentPosition(position);
 
-        this.el.addEventListener('mouseleave', function () {
-            let texto = document.querySelector("#texto");
-            texto.setAttribute("visible", false);
-        });
+    function position(position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        var map = L.map('map').setView([lat, long], 17);
+        L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib3ZvY29sb3JpZG8iLCJhIjoiY2wybjdnM3QwMTRyajNqbXhmZjhiZnY4MCJ9.IJAI7VXyE6-R8jQHoylX4A", {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1
+        }).addTo(map);
+        marker = L.marker([-1.43815, -48.46054]).addTo(map);
+        marker.bindPopup("<b>IFPA Campus Belém<b>").openPopup();
+
+        marker.on("dblclick", (e) => {
+            window.location.href = "Mapa.html";
+        })
     }
-});
+}
